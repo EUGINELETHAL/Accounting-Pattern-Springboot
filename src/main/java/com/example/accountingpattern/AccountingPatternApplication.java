57358ff803486c58519cbb94266aa339c553f5b2
Eugine <ochungeugine@gmail.com>
2024-10-13 01:03:06 +0300

    initialized project

---
+++ src/main/java/com/example/accountingpattern/AccountingPatternApplication.java
@@ -0,0 +1,13 @@
+package com.example.accountingpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountingPatternApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AccountingPatternApplication.class, args);
+    }
+
+}
